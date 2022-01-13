81c4d7d734cccfda09e7b81d0174829816222e39
유명준 <mj.yu@ilts.co.kr>
2022-01-13 17:43:16 +0900

    퍼블리싱 1차

--- mini-curly-fe/src/App.js
+++ mini-curly-fe/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './components/header';
+import Header from './components/Header';
+import './css/App.css';
 
 function App() {
   return (
--- mini-curly-fe/src/components/header.js
+++ mini-curly-fe/src/components/header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Categorys from "./Categorys";
+import logo from "../images/logo.png";
 
-function header() {
+function Header() {
     return (
         <>
-            <div className="header__warpper">
+            <header className="header__warpper">
                 <div className="top__wrapper">
                     <div className="top__wrapper__left">
                         <button className="shippng__information__btn">샛별-택배 배송안내 &gt; </button>
@@ -14,9 +16,19 @@ function header() {
                         <button className="customer__service__center">고객센터</button>
                     </div>
                 </div>
-            </div>
+                <div className="header__img">
+                    <img src={logo} alt="~" />
+                </div>
+                <div className="category__wrapper">
+                    <Categorys />
+                    <a href="#" className="new__product">신상품</a>
+                    <a href="#" className="new__product">신상품</a>
+                    <a href="#" className="new__product">신상품</a>
+                    <div className="search__wrapper">검색</div>
+                </div>
+            </header>
         </>
     );
 }
 
-export default header;
+export default Header;
