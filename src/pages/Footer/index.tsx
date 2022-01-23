959099f6862447fa6eca9bc2399592cf6ae4f75b
YuMyeongJun <y1207cc@gmail.com>
2022-01-23 23:31:15 +0900

    footer 1차 작업

--- src/pages/Footer/index.tsx
+++ src/pages/Footer/index.tsx
@@ -2,7 +2,70 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer></footer>
+        <footer>
+            <div className="footer__top">
+                <div className="inner__footer">
+                    <div className="left__wrapper">
+                        <div className="title">고객행복센터</div>
+                        <div className="detail__wrapper">
+                            <div className="detail__left">1644-1107</div>
+                            <div className="detail__right">
+                                <span>365고객센터</span>
+                                <span>오전7시 ~ 오후 7시</span>
+                            </div>
+                        </div>
+                        <div className="detail__wrapper">
+                            <div className="detail__left">카카오톡 문의</div>
+                            <div className="detail__right">
+                                <span>365고객센터</span>
+                                <span>오전7시 ~ 오후 7시</span>
+                            </div>
+                        </div>
+                        <div className="detail__wrapper">
+                            <div className="detail__left">1:1 문의</div>
+                            <div className="detail__right">
+                                <span>25시간 접수 가능</span>
+                                <span>고객센터 운영시간에 순차적으로 답변해드리겠습니다.</span>
+                            </div>
+                        </div>
+                        <div className="detail__wrapper">
+                            <div className="detail__left">대량주문 문의</div>
+                            <div className="detail__right">
+                                <span></span>
+                                <span>비회원의 경우 메일로 문의 바랍니다.</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right__wrapper">
+                        <div className="title__link">
+                            <link>컬리소개</link>
+                            <link>컬리소개영상</link>
+                            <link>인재채용</link>
+                            <link>이용약관</link>
+                            <link>개인정보처리방침</link>
+                            <link>이용안내</link>
+                        </div>
+                        <div className="detail__wrapper">
+                            <div className="">법인명 (상호): 주식회사 미니컬리 | 사업자등록번호: 111-11-11111 <a href="#">사업자정보 확인</a></div>
+                            <div className="">통신판매업:제 2018-서울강남-00000호 | 개인정보보호책임자: 이종섭</div>
+                            <div className="">주소: 서울턱별시 간낭구 테란헤로 123, 2층(역샘동) | 대표이사: 박시온</div>
+                            <div className="">입점문의: <a href="#">입점문의하기</a> | 마케팅제휴:<a href="#">y120@gmail.com</a></div>
+                            <div className="">채용문의:<a href="#">y120@gmail.com</a></div>
+                        </div>
+                        <div className="sns__link">
+                            <ul>
+                                <li>인스타그램</li>
+                                <li>페이스북</li>
+                                <li>블로그</li>
+                                <li>유튜브</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer__link"></div>
+            </div>
+            <div className="footer__bottom"></div>
+        </footer>
     )
 }
 
