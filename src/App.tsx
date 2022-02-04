4b501678e0c2a600fcf6aa333a09cd40b0ba4c3a
유명준 <mj.yu@ilts.co.kr>
2022-02-04 22:45:02 +0900

    노드 서버 구성 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import './css/App.css';
 const param = new URLSearchParams();
 param.append('memberId', 'sion1234');
 param.append('password', 'Aa123!12311#');
-axios.post('/api', param)
+axios.post('/api/login', param)
   .then((res) => {
     console.log(res)
   })
