ca800290ced40124da19682fe78efbeea376e5a1
유명준 <mj.yu@ilts.co.kr>
2022-02-05 23:46:57 +0900

    proxy 주소 설정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'ec2-52-78-116-115.ap-northeast-2.compute.amazonaws.com:8080',
+            target: 'http://ec2-52-78-116-115.ap-northeast-2.compute.amazonaws.com:8080',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': ''
