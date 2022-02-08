6b34e33ca5f75bf551669e61b16418d51fe790f2
YuMyeongJun <y1207cc@gmail.com>
2022-02-08 23:08:50 +0900

    로그인 세션 초기 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header, Menus, Footer, MainBody, SignUp, SignIn } from './pages';
 import './css/App.css';
 
-const param = new URLSearchParams();
-param.append('memberId', 'superadmin');
-param.append('password', 'Admin1234!@#$');
 
-axios.post('/api/login', param)
-  .then((res) => {
-    console.log(res)
-  })
-  .catch((e) => {
-    console.log(e);
-  })
 
 function App() {
+
   return (
     <BrowserRouter>
       <Header />
@@ -31,5 +21,4 @@ function App() {
 
   );
 }
-
 export default App;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import './style.css';
 
 function SignIn() {
+
+    const [account, setAccount] = useState({
+        memberId: "",
+        password: ""
+    });
+
+    const fetchLogin = (account: any) => {
+        const param = new URLSearchParams();
+        param.append('memberId', account.memberId);
+        param.append('password', account.password);
+        axios.post('/api/login', account)
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+    }
+
+
+    const onSubmit = async () => {
+        try {
+            const user = await fetchLogin(account);
+            console.log(user);
+            //성공하면 해당 user 아이디 패스워드값 셋팅
+            // localStorage.getItem('memberId', user.memberId);
+            // localStorage.getItem('password', user.password);
+        } catch (error) {
+
+            //실패하면 throw new Error("") 값 출력
+            alert(error);
+        }
+    };
     return (
         <section className="sign__in__container">
             <article className="header__title">
@@ -20,7 +54,7 @@ function SignIn() {
                             <Link to="/SignIn" className="link">비밀번호 찾기</Link>
                         </div>
                     </div>
-                    <button type="submit" className="btn btn_login">로그인</button>
+                    <button type="button" className="btn btn_login" onClick={onSubmit}>로그인</button>
                 </form>
                 <Link to="/SignUp" className="btn btn_signUp"><span>회원가입</span></Link>
             </article>
